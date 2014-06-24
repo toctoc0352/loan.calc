d56792ead20c3d0e14f7294724bc84d133aaf939
江上剛志 <egm352@gmail.com>
2014-06-24 23:45:40 +0900

    毎月返済額の処理を追加

--- app/models/payment_per_month.rb
+++ app/models/payment_per_month.rb
@@ -7,4 +7,16 @@ class PaymentPerMonth
   validates :bonus, presence: true, numericality: true
   validates :period, presence: true, numericality: true
   validates :apy, presence: true, numericality: true
+  
+  def amount
+    if !self.valid?
+      return nil
+    end
+    apy = self.apy / 100.to_f
+    ratePerMonth = apy / 12.to_f
+    repaymentCount = self.period * 12.to_f
+    repaymentAmount = ( (self.loan - self.bonus) * ratePerMonth ) / ( 1 - ( 1 + ratePerMonth ) ** (repaymentCount * -1) ).to_f
+    repaymentAmount.round
+  end
+  
 end
--- spec/models/payment_per_month_spec.rb
+++ spec/models/payment_per_month_spec.rb
@@ -2,50 +2,85 @@ require 'spec_helper'
 
 describe PaymentPerMonth do
   before do
-     @PaymentPerMonth = PaymentPerMonth.new(loan: 1000000, bonus: 200000,
+     @paymentPerMonth = PaymentPerMonth.new(loan: 1000000, bonus: 200000,
                             period: 10, apy: 1.0)
   end
   
-  subject { @PaymentPerMonth }
+  subject { @paymentPerMonth }
   
   describe "when loan is not present" do
-    before { @PaymentPerMonth.loan = "" }
+    before { @paymentPerMonth.loan = "" }
     it { should_not be_valid }
   end
   
   describe "when loan is not number" do
-    before { @PaymentPerMonth.loan = "hoge" }
+    before { @paymentPerMonth.loan = "hoge" }
     it { should_not be_valid }
   end
 
   describe "when bonus is not present" do
-    before { @PaymentPerMonth.bonus = "" }
+    before { @paymentPerMonth.bonus = "" }
     it { should_not be_valid }
   end
   
   describe "when bonus is not number" do
-    before { @PaymentPerMonth.bonus = "hoge" }
+    before { @paymentPerMonth.bonus = "hoge" }
     it { should_not be_valid }
   end
   
   describe "when period is not present" do
-    before { @PaymentPerMonth.period = "" }
+    before { @paymentPerMonth.period = "" }
     it { should_not be_valid }
   end
   
   describe "when period is not number" do
-    before { @PaymentPerMonth.period = "hoge" }
+    before { @paymentPerMonth.period = "hoge" }
     it { should_not be_valid }
   end
 
   describe "when apy is not present" do
-    before { @PaymentPerMonth.apy = "" }
+    before { @paymentPerMonth.apy = "" }
     it { should_not be_valid }
   end
   
   describe "when apy is not number" do
-    before { @PaymentPerMonth.apy = "hoge" }
+    before { @paymentPerMonth.apy = "hoge" }
     it { should_not be_valid }
+  end
+  
+  describe "valid amount" do
+    
+    describe "return 126481" do
+      before do
+        @paymentPerMonth.loan = 30000000
+        @paymentPerMonth.bonus = 0
+        @paymentPerMonth.period = 30
+        @paymentPerMonth.apy = 3
+      end
+      its(:amount){ should == 126481 }
+    end
+
+
+    describe "return 80410" do
+      before do
+        @paymentPerMonth.loan = 30000000
+        @paymentPerMonth.bonus = 5000000
+        @paymentPerMonth.period = 30
+        @paymentPerMonth.apy = 1
+      end
+      its(:amount){ should == 80410 }
+    end
+
+  end
+
+  describe "invalid amount" do
+      before do
+        @paymentPerMonth.loan = nil
+        @paymentPerMonth.bonus = nil
+        @paymentPerMonth.period = nil
+        @paymentPerMonth.apy = nil
+      end
+      its(:amount){ should == nil }
   end
   
 end
