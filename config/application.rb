2b46988e1da3a2edcdc6e522390bd850639291fa
江上剛志 <egm352@gmail.com>
2014-09-15 23:46:28 +0900

    ラベルの日本語化

--- config/application.rb
+++ config/application.rb
@@ -24,8 +24,8 @@ module LoanCalc
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
     config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
