1b4b8cbc4d0be98574f6db34e934f377a3adf815
江上剛志 <egm352@gmail.com>
2014-09-21 00:33:56 +0900

    エラーメッセージを表示（レイアウト調整は未完了）

--- app/controllers/payment_per_months_controller.rb
+++ app/controllers/payment_per_months_controller.rb
@@ -1,6 +1,6 @@
 class PaymentPerMonthsController < ApplicationController
   
-  respond_to :html, :js
+  respond_to :html
   
   def new
     @paymentPerMonth = PaymentPerMonth.new
@@ -9,7 +9,11 @@ class PaymentPerMonthsController < ApplicationController
   
   def create
     @paymentPerMonth = PaymentPerMonth.new(permit_params)
-    respond_with @paymentPerMonth
+    if @paymentPerMonth.valid?
+    else
+      render 'new'
+    end
+    #respond_with @paymentPerMonth, location: new_payment_per_month_url
   end
   
   private
